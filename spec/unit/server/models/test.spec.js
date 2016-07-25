c9d44d280b4d7adeba00f812f387dc8ebe6736d7
Michael Harrison <harrim91@hotmail.co.uk>
2016-07-25 16:50:57 +0100

    structure of test directories

--- spec/unit/server/models/test.spec.js
+++ spec/unit/server/models/test.spec.js
@@ -1,4 +1,4 @@
-var app = require('../../../server.js'),
+var app = require('../../../../server.js'),
     should = require('should'),
     mongoose = require('mongoose'),
     Sample = mongoose.model('Sample');
